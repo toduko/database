4d9724cc163599fc14f9dd1eccc856f2eb012ef2
toduko <teddyepich@gmail.com>
2022-05-31 10:17:52 +0300

    Added project skeleton

---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+  std::cout << "Database" << std::endl;
+
+  return 0;
+}
---
+++ src/test.cpp
@@ -0,0 +1,4 @@
+#include "../acutest/include/acutest.h"
+
+TEST_LIST = {
+    {NULL, NULL}};
